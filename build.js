3e5f0000b84c05d63013a7ccece123aad33317c8
bredele <olivier.wietrich@gmail.com>
2016-07-01 13:12:28 -0600

    updated example

--- build.js
+++ build.js
@@ -523,18 +523,22 @@ function connect(other, node) {
   return function(ctx) {
 
     ctx.on('candidate', function(candidate) {
+      console.log('master', 'created candidate ans transmit to slave peer')
       other.ice(candidate);
     });
 
     other.on('candidate', function(candidate) {
+      console.log('slave', 'created candidate ans transmit to master peer')
       ctx.ice(candidate);
     });
 
     other.on('answer', function(offer) {
+      console.log('slave', 'got offer from master')
       ctx.remote(offer);
     });
 
     ctx.on('offer', function(offer) {
+      console.log('slave', 'got answer from slave')
       other.remote(offer);
       other.answer();
     });
